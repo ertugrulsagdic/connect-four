0bf0adf6885428d28e22574ec82122969b70e473
ertugrulsagdic <38252389+ertugrulsagdic@users.noreply.github.com>
2021-01-15 17:18:34 +0300

    board

---
+++ board.py
@@ -0,0 +1,130 @@
+import numpy as np
+import sys
+import math
+import pygame
+
+
+class Board:
+    def __init__(self):
+        # colors
+        self.white = (255, 255, 255)
+        self.black = (0, 0, 0)
+        self.red = (255, 0, 0)
+        self.yellow = (255, 255, 0)
+
+        # board
+        self.rows = 6
+        self.columns = 7
+
+        # create empty board
+        self.board = self.create_board()
+
+        # square size of the screen
+        self.square_size = 100
+        # piece radius of the screen
+        self.radius = int(self.square_size / 2 - 5)
+
+        # height and width of the screen
+        self.width = self.columns * self.square_size
+        self.height = (self.rows + 1) * self.square_size
+
+        # size of the screen
+        self.size = (self.width, self.height)
+
+        # set screen for the game
+        self.screen = pygame.display.set_mode(self.size)
+
+    # create board with zeros
+    def create_board(self):
+        board = np.zeros((self.rows, self.columns))
+        return board
+
+    # place the piece on board
+    def drop_piece(self, row, column, piece):
+        self.board[row][column] = piece
+
+    # check if the place for piece empty
+    def is_empty(self, column):
+        if self.board[self.rows - 1][column] == 0:
+            return True
+        else:
+            return False
+
+    def get_next_row(self, column):
+        for row in range(self.rows):
+            if self.board[row][column] == 0:
+                return row
+
+    def print_board(self):
+        print(np.flip(self.board, 0))
+
+    def wining_move(self, piece):
+        # Check horizontal locations for win
+        for column in range(self.columns - 3):
+            for row in range(self.rows):
+                if self.board[row][column] == piece \
+                        and self.board[row][column + 1] == piece \
+                        and self.board[row][column + 2] == piece \
+                        and self.board[row][column + 3] == piece:
+                    return True
+
+        # Check vertical locations for win
+        for column in range(self.columns):
+            for row in range(self.rows - 3):
+                if self.board[row][column] == piece \
+                        and self.board[row + 1][column] == piece \
+                        and self.board[row + 2][column] == piece \
+                        and self.board[row + 3][column] == piece:
+                    return True
+
+        # Check positively sloped diagonals
+        for column in range(self.columns - 3):
+            for row in range(self.rows - 3):
+                if self.board[row][column] == piece \
+                        and self.board[row + 1][column + 1] == piece \
+                        and self.board[row + 2][column + 2] == piece \
+                        and self.board[row + 3][column + 3] == piece:
+                    return True
+
+        # Check negatively sloped diagonals
+        for column in range(self.columns - 3):
+            for row in range(3, self.rows):
+                if self.board[row][column] == piece \
+                        and self.board[row - 1][column + 1] == piece \
+                        and self.board[row - 2][column + 2] == piece \
+                        and self.board[row - 3][column + 3] == piece:
+                    return True
+
+    def draw_board(self, player_1, player_2):
+        for column in range(self.columns):
+            for row in range(self.rows):
+                pygame.draw.rect(self.screen, self.white, (
+                    column * self.square_size, row * self.square_size + self.square_size, self.square_size,
+                    self.square_size))
+                pygame.draw.circle(self.screen, self.black, (
+                    int(column * self.square_size + self.square_size / 2),
+                    int(row * self.square_size + self.square_size + self.square_size / 2)), self.radius)
+
+        for column in range(self.columns):
+            for row in range(self.rows):
+                if self.board[row][column] == player_1:
+                    pygame.draw.circle(
+                        self.screen,
+                        self.red,
+                        (
+                            int(column * self.square_size + self.square_size / 2),
+                            self.height - int(row * self.square_size + self.square_size / 2)
+                        ),
+                        self.radius
+                    )
+                elif self.board[row][column] == player_2:
+                    pygame.draw.circle(
+                        self.screen,
+                        self.yellow,
+                        (
+                            int(column * self.square_size + self.square_size / 2),
+                            self.height - int(row * self.square_size + self.square_size / 2)
+                        ),
+                        self.radius
+                    )
+        pygame.display.update()
