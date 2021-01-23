006f27e3f87d503ee6997b56ddf4b9486b87a51d
Arda Bayram <56687124+arda-bayram@users.noreply.github.com>
2021-01-23 18:18:51 +0300

    empty commit

--- minimax_alpha_beta.py
+++ minimax_alpha_beta.py
@@ -3,6 +3,7 @@ import random
 from board import *
 from evaluations import *
 
+
 def get_valid_locations(board):
     valid_locations = []
     for column in range(board.columns):
@@ -32,34 +33,36 @@ def minimax_alpha_beta(board, depth, alpha, beta, maximizingPlayer, piece):
         else:  # Depth is zero
             return None, evaluation3(board, piece)
     if maximizingPlayer:
-        value = -math.inf
-        column = random.choice(valid_locations)
+        value=-math.inf
+        column=random.choice(valid_locations)
         for col in valid_locations:
-            row = board.get_next_row(col)
-            b_copy = Board()
-            b_copy.board = board.board.copy()
+            row=board.get_next_row(col)
+            b_copy=Board()
+            b_copy.board=board.board.copy()
             b_copy.drop_piece(row, col, piece)
-            new_score = minimax_alpha_beta(b_copy, depth - 1, alpha, beta, False, piece)[1]
+            new_score=minimax_alpha_beta(
+                b_copy, depth - 1, alpha, beta, False, piece)[1]
             if new_score > value:
-                value = new_score
-                column = col
-            alpha = max(alpha, value)
+                value=new_score
+                column=col
+            alpha=max(alpha, value)
             if alpha >= beta:
                 break
         return column, value
     else:  # Minimizing player
-        value = math.inf
-        column = random.choice(valid_locations)
+        value=math.inf
+        column=random.choice(valid_locations)
         for col in valid_locations:
-            row = board.get_next_row(col)
-            b_copy = Board()
-            b_copy.board = board.board.copy()
+            row=board.get_next_row(col)
+            b_copy=Board()
+            b_copy.board=board.board.copy()
             b_copy.drop_piece(row, col, opponent_piece)
-            new_score = minimax_alpha_beta(b_copy, depth - 1, alpha, beta, True, piece)[1]
+            new_score=minimax_alpha_beta(
+                b_copy, depth - 1, alpha, beta, True, piece)[1]
             if new_score < value:
-                value = new_score
-                column = col
-            beta = min(beta, value)
+                value=new_score
+                column=col
+            beta=min(beta, value)
             if alpha >= beta:
                 break
         return column, value
